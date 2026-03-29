074305783fd5cdf03bae892b55821fa39b3ff1e0
Thomas8207 <leontorresthomas@gmail.com>
2026-03-29 18:28:14 -0500

    Primera implementación MVP del sistema de rutinas

---
+++ Proyecto_rutinas.py
@@ -0,0 +1,23 @@
+class Ejercicio:
+    def __init__(self, nombre, grupo_muscular, series, repeticiones):
+        self.nombre = nombre
+        self.grupo_muscular = grupo_muscular
+        self.series = series
+        self.repeticiones = repeticiones
+
+    def mostrar_detalle(self):
+        return f"{self.nombre} - {self.series}x{self.repeticiones} ({self.grupo_muscular})"
+
+
+class DiaEntrenamiento:
+    def __init__(self, nombre_dia):
+        self.nombre_dia = nombre_dia
+        self.ejercicios = []
+
+    def agregar_ejercicio(self, ejercicio):
+        self.ejercicios.append(ejercicio)
+
+    def mostrar_ejercicios(self):
+        print(f"\n--- {self.nombre_dia} ---")
+        for ejercicio in self.ejercicios:
+            print(ejercicio.mostrar_detalle())
