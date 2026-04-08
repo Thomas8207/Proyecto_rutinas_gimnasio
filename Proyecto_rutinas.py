4327045c7e2d481237ff455178f56f330a1c3725
Andres Felipe Zora <andresfelipezora@gmail.com>
2026-04-08 02:22:18 +0000

    Implemente clase Usuario

--- Proyecto_rutinas.py
+++ Proyecto_rutinas.py
@@ -21,4 +21,20 @@ class DiaEntrenamiento:
         print(f"\n--- {self.nombre_dia} ---")
         for ejercicio in self.ejercicios:
             print(ejercicio.mostrar_detalle())
+class Usuario:
+    def __init__(self, nombre: str, edad: int, peso: float, objetivo: str, nivel: str, dias_disponibles: int):
+
+        if edad <= 0 or peso <= 0:
+            raise ValueError("Edad y peso deben ser valores positivos")
+
+        self.nombre: str = nombre
+        self.edad: int = edad
+        self.peso: float = peso
+        self.objetivo: str = objetivo
+        self.nivel: str = nivel
+        self.dias_disponibles: int = dias_disponibles
+
+    def __str__(self):
+        return f"Usuario: {self.nombre} | Objetivo: {self.objetivo}"
+        
       
